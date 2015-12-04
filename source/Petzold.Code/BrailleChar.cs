9235dee2525dbef3c5cd97376c2cf42f932dfeee
Michael Phillips <github@idiotandrobot.com>
2015-12-04 18:48:18 +0000

    Add Petzold.Code project

--- source/Petzold.Code/BrailleChar.cs
+++ source/Petzold.Code/BrailleChar.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace Braille
+namespace Petzold.Code
 {
     public class BrailleChar : IEnumerable<bool>
     {
--- source/Petzold.Code/MorseChar.cs
+++ source/Petzold.Code/MorseChar.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Braille
+namespace Petzold.Code
 {
     public class MorseChar : IEnumerable<bool>
     {
